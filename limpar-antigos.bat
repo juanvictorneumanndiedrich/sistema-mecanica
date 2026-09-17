b1547a862c4d518e066aa8778b06fc5a565dcfb1
juan victor neumann diedrich <neumanndiedrichj@gmail.com>
2026-09-17 09:17:20 -0300

    Apagar arquivos órfãos de Cierre de Proveedor
    Elimina CierreProveedor.java, CierreProveedorDAO.java, CierreProveedorController.java, CierreProveedorDialog.java, FormaPagoCompra.java y EstadoCierreProveedor.java, clases huerfanas que ya no eran usadas por ninguna pantalla (el flujo de cierre de cuenta de proveedor fue reemplazado por Compra). Incluye el script auxiliar limpar-antigos.bat usado para la limpieza.

--- limpar-antigos.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0src\main\java\com\mecanica"
-echo Apagando os arquivos antigos em portugues...
-echo.
-del /s /q *Equipamento*.java *Fornecedor*.java *Funcionario*.java *Servico*.java *Movimento*.java *Retirada*.java FormaPagamentoCompra.java
-echo.
-echo Pronto. Agora no Eclipse: F5 no projeto e depois Project ^> Clean.
-pause
